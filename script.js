56e6f114d30489da3143550c0ccccb190d6975cf
aadesh_shetty <aadeshshetty123@gmail.com>
2022-10-11 13:51:22 +0530

    Second level changes are made

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const addBtn = document.getElementById("add");
 
 const notes = JSON.parse(localStorage.getItem("notes"));
 
+var headarr = JSON.parse(localStorage.getItem("heads"));
+
+const helpBtn = document.getElementById("help");
+
 //EVENT LISTENER
 
 addBtn.addEventListener("click", (e) => {
@@ -12,27 +16,37 @@ addBtn.addEventListener("click", (e) => {
 //FUNCTION
 
 if (notes) {
-	notes.forEach((note) => {
-		addNewNote(note);
+	const len=notes.length;
+	// console.log(len);
+	notes.forEach((note,index) => {
+		addNewNote(note,index);
 	});
 }
 
-function addNewNote(text = "") {
+
+function addNewNote(text = "",index) {
 	const note = document.createElement("div");
 	note.classList.add("note");
 	note.innerHTML = `
         <div class="notes">
-			<div class="tools">
-				<button class="edit"><i class="fas fa-edit"></i></button>
-				<button class="delete"><i class="fas fa-trash-alt"></i></button>
+			<div class="head-container">
+				<div class="head">
+					<a class="heading">${index<headarr.length?headarr[index]:"Note"}</a>
+				</div>
+				<div class="tools">
+					<button class="download"><i class="fas fa-download"></i></button>
+					<button class="edit"><i class="fas fa-edit"></i></button>
+					<button class="delete"><i class="fas fa-trash-alt"></i></button>
+				</div>
 			</div>
 			<div class="text ${text ? "" : "hidden"}"></div>
-			<textarea class="${text ? "hidden" : ""}"></textarea>
+			<textarea class="area ${text ? "hidden" : ""}"></textarea>
 		</div>
         `;
-
+	const downloadBtn = note.querySelector(".download");
 	const editBtn = note.querySelector(".edit");
 	const deleteBtn = note.querySelector(".delete");
+	const heading=note.querySelector(".heading");
 
 	const main = note.querySelector(".text");
 	const textArea = note.querySelector("textarea");
@@ -42,9 +56,35 @@ function addNewNote(text = "") {
 
 	//EVENT LISTENER
 
+	heading.addEventListener("dblclick", (e) =>{
+		var head= prompt("Enter name here");
+		if(head!=null){
+		note.querySelector(".heading").innerHTML=head;
+		updateheadLs(note);
+		}
+	});
+
+	downloadBtn.addEventListener("click", (e) => {
+		const content = textArea.value;
+		var currNote=note;
+		if(content){
+		downloadNote(content,currNote);
+		}
+	});
+
 	deleteBtn.addEventListener("click", (e) => {
-		note.remove();
-		updateLS();
+		var con = confirm("Are you sure want to delete the note?");
+		if(con){
+			const head=note.querySelector('.text').innerText;
+			// console.log(JSON.parse(localStorage.getItem("notes")).indexOf(head),JSON.parse(localStorage.getItem("notes")),head.replace(/\r?\n|\r/g, " "));
+			const index=JSON.parse(localStorage.getItem("notes")).indexOf(head);
+			var arr=JSON.parse(localStorage.getItem("heads"))
+			arr.splice(index,1);
+			localStorage.setItem("heads", JSON.stringify(arr));
+			// console.log(arr,JSON.parse(localStorage.getItem("heads")));
+			note.remove();
+			updateLS();
+		}
 	});
 
 	editBtn.addEventListener("click", () => {
@@ -54,6 +94,7 @@ function addNewNote(text = "") {
 
 	textArea.addEventListener("input", (e) => {
 		const { value } = e.target;
+		const currNote = note;
 		main.innerHTML = marked(value);
 		updateLS();
 	});
@@ -61,14 +102,58 @@ function addNewNote(text = "") {
 	document.body.appendChild(note);
 }
 
+function downloadNote(content,currNote) {
+	const ele = document.createElement('a');
+	const blob = new Blob([content],{type : 'plain/text'});
+	const fileUrl=URL.createObjectURL(blob);
+	const head=currNote.querySelector(".heading");
+	ele.setAttribute('href',fileUrl);
+	ele.setAttribute('download',head.innerText+".txt");
+	ele.style.display='none';
+	document.body.appendChild(ele);
+	ele.click();
+
+	document.body.removeChild(ele);
+};
+
 function updateLS() {
+	
 	const notesText = document.querySelectorAll("textarea");
-
-	const notes = [];
-
+	const notes=[]
+	
 	notesText.forEach((note) => {
 		notes.push(note.value);
 	});
-
 	localStorage.setItem("notes", JSON.stringify(notes));
+
+}
+
+var heads=[];
+function updateheadLs(currNote){
+	if(localStorage.getItem('heads')){
+		console.log(JSON.parse(localStorage.getItem('heads')));
+		heads=JSON.parse(localStorage.getItem('heads'));
+		
+	}
+	const head = currNote.querySelector('.heading');
+	
+	// console.log(head.innerText);
+	heads.push(head.innerText);
+	localStorage.setItem("heads",JSON.stringify(heads));
+}
+
+const hel = document.querySelector('.help-btn');
+document.addEventListener('mouseup', function (e) {
+	var container = document.querySelector('.h');
+
+	if (!container.classList.contains("hide") && !(container.contains(e.target))) {
+		container.classList.add("hide");
+	}
+	else if (e.target == hel) {
+		container.classList.remove("hide");
+	}
+});
+
+function headingAlert(){
+	
 }
